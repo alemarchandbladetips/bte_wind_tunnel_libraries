4190156a7c7c0610f410e518378cda7cfca7ba3d
Unknown <antoinelemarchand@gmail.com>
2022-06-09 15:07:03 +0200

    add wind tunnel class

---
+++ bte_wind_tunnel.cpp
@@ -0,0 +1,139 @@
+
+#include "bte_wind_tunnel.h"
+
+#define DEFAULT_MAX_CELL_POWER 2750.0
+#define POWER_SUPPLIER_MAX_POWER 3000.0
+
+#define MAX_PWM 2000
+#define MIN_PWM 1000
+
+ 
+bte_wind_tunnel::bte_wind_tunnel()
+{
+	cell[0].set_all_params(0,*_pwm_driver0,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	cell[1].set_all_params(7,*_pwm_driver0,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	cell[2].set_all_params(0,*_pwm_driver1,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	cell[3].set_all_params(7,*_pwm_driver1,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	cell[4].set_all_params(0,*_pwm_driver2,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	cell[5].set_all_params(7,*_pwm_driver2,MIN_PWM,MAX_PWM,DEFAULT_MAX_CELL_POWER);
+	_central_motor.set_all_params(15,*_pwm_driver0,MIN_PWM,MAX_PWM);
+	_enable_flag = 0;
+}
+	  
+	 
+int8_t bte_wind_tunnel::set_max_cell_power(float max_cell_power)
+{
+	uint8_t i;
+	
+	if(max_cell_power>POWER_SUPPLIER_MAX_POWER)
+	{
+		return -1;
+	}
+	
+	for(i=0;i<6;i++)
+	{
+		_cell[i].set_max_power(max_cell_power);
+	}
+	
+	return 0;
+}
+	  
+	  
+int8_t bte_wind_tunnel::power_individual_motor(uint8_t cell_number, uint8_t motor_number, float thrust_level)
+{
+	return _cell[cell_number].power_individual_motor(motor_number,thrust_level);
+}
+	  
+int8_t bte_wind_tunnel::power_rank0_motors(float thrust_level)
+{
+	int8_t retval = 0;
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		retval += _cell[i].power_rank0_motors(thrust_level);
+	}
+	retval += _central_motor.set_thrust_level(thrust_level);
+	
+	return retval;
+}
+	  
+int8_t bte_wind_tunnel::power_rank1_motors(float thrust_level)
+{
+	int8_t retval = 0;
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		retval += _cell[i].power_rank1_motors(thrust_level);
+	}
+	retval += _central_motor.set_thrust_level(thrust_level);
+	
+	return retval;
+}
+
+int8_t bte_wind_tunnel::power_rank2_motors(float thrust_level)
+{
+	int8_t retval = 0;
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		retval += _cell[i].power_rank2_motors(thrust_level);
+	}
+	retval += _central_motor.set_thrust_level(thrust_level);
+	
+	return retval;
+}
+	  
+void bte_wind_tunnel::power_off_all_motors()
+{
+	uint8_t i;
+
+	for(i=0;i<6;i++)
+	{
+		_cell[i].power_off_all_motors();
+	}
+	_central_motor.power_off();
+}
+	  
+void bte_wind_tunnel::enable()
+{
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		_cell[i].enable();
+	}
+	_central_motor.enable();
+}
+	  
+void bte_wind_tunnel::disable()
+{
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		_cell[i].disable();
+	}
+	_central_motor.disable();
+}
+	  
+float bte_wind_tunnel::get_current_cell_power(uint8_t cell_number)
+{
+	return _cell[cell_number].get_current_power();
+}
+	  
+float bte_wind_tunnel::get_current_total_power()
+{
+	float total_power = 0;
+	uint8_t i;
+	
+	for(i=0;i<6;i++)
+	{
+		total_power += _cell[i].get_current_power();
+	}
+	total_power += _central_motor.get_current_power();
+	
+	return total_power;
+}
---
+++ bte_wind_tunnel.h
@@ -0,0 +1,105 @@
+
+#include <Arduino.h>
+#include "bte_wind_tunnel_cell.h"
+
+class bte_wind_tunnel{
+  /**
+   * \class bte_wind_tunnel
+   * \brief class used to instantiate the 37 motors wind tunnel designed by Bladetips Energy
+   */
+  
+  public:
+  
+	  /**
+       * \fn bte_wind_tunnel()
+	   * \brief Constructor for the bte_wind_tunnel class
+       */
+	  bte_wind_tunnel();
+	  
+	  
+	   /**
+       * \fn int8_t set_max_cell_power(float max_cell_power)
+	   * \brief Set the maximum power for all individual cells of the wind tunnel
+       * @param (float)max_cell_power maximum power for all cells individually (W)
+       * @return -1 if the function fails, 0 otherwise
+       */
+	  int8_t set_max_cell_power(float max_cell_power);
+	  
+	  
+	  /**
+	   * \fn int8_t power_individual_motor(uint8_t cell_number, uint8_t motor_number, float thrust_level)
+       * \brief Set the thrust level for a particular motor
+       * @param (uint8_t)cell_number 0 for the center motor 1 for the upper left cell, counting goes clockwise
+	   * @param (uint8_t)motor_number (0 to 5) 0 for the motor closer to the center, 1 and 2 for the second line, 3, 4 and 5 for the 3rd line (counting clockwise)
+	   * @param (float)thrust_level desired thrust level [0-1], 0 motor off, 1 motor at full speed.
+       * @return -1 if the power exceed total or cell power, -2 if motor or cell doesn't exist, 0 otherwise
+       */
+	  int8_t power_individual_motor(uint8_t cell_number, uint8_t motor_number, float thrust_level);
+	  
+	  /**
+	   * \fn  int8_t power_rank0_motors(float thrust_level)
+       * \brief Set the thrust level for the motors (7 motors) of the inner circle of the wind tunnel
+	   * @param (float)thrust_level desired thrust level [0-1], 0 motor off, 1 motor at full speed.
+       * @return -1 if the power exceed total or cell power, 0 otherwise
+       */
+	  int8_t power_rank0_motors(float thrust_level);
+	  
+	  /**
+	   * \fn int8_t power_rank1_motors(float thrust_level)
+       * \brief Set the thrust level for the motors (13 motors) of the inner and second circle of the wind tunnel
+	   * @param (float)thrust_level desired thrust level [0-1], 0 motor off, 1 motor at full speed.
+       * @return -1 if the power exceed total or cell power, 0 otherwise
+       */
+	  int8_t power_rank1_motors(float thrust_level);
+	  
+	  /**
+	   * \fn int8_t power_rank2_motors(float thrust_level)
+       * \brief Set the thrust level for the motors (37 motors) of the inner and second and third circle of the wind tunnel
+	   * @param (float)thrust_level desired thrust level [0-1], 0 motor off, 1 motor at full speed.
+       * @return -1 if the power exceed total or cell power, 0 otherwise
+       */
+	  int8_t power_rank2_motors(float thrust_level);
+	  
+	  /**
+	   * \fn void power_off_all_motors()
+       * \brief power off all motors, do not disable the motors.
+       */
+	  void power_off_all_motors();
+	  
+	  /**
+	   * \fn void enable()
+       * \brief enable all motors. Must be called before using the wind tunnel (disable by default)
+       */
+	  void enable();
+	  
+	  /**
+	   * \fn void disable()
+       * \brief disable all motors. Enable must be called if you want to use motor after.
+       */
+	  void disable();
+	  
+	  /**
+	   * \fn float get_current_cell_power(uint8_t cell_number)
+       * \brief Get the current power of a particular cell
+	   * @param (uint8_t)cell_number number of the cell we want the power returned
+       * @return (float)power the current power of the cell (W)
+       */
+	  float get_current_cell_power(uint8_t cell_number);
+	  
+	  /**
+	   * \fn float get_current_total_power()
+       * \brief Set the thrust level for the motors (37 motors) of the inner and second and third circle of the wind tunnel
+       * @return (float)power the current power of all the cells (W)
+       */
+	  float get_current_total_power();
+  
+
+  private:
+	  Adafruit_PWMServoDriver _pwm_driver0 = Adafruit_PWMServoDriver(0x40);
+	  Adafruit_PWMServoDriver _pwm_driver1 = Adafruit_PWMServoDriver(0x41);
+	  Adafruit_PWMServoDriver _pwm_driver2 = Adafruit_PWMServoDriver(0x42);
+	  bte_wind_tunnel_cell _cell[6];
+	  bte_wind_tunnel_motor _central_motor;
+	  uint8_t _enable_flag = 0;
+  
+  };
