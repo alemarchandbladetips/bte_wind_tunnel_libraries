11fff9a71cc346145bbe79e59df74d8c1f538a93
Unknown <antoinelemarchand@gmail.com>
2022-06-09 15:00:41 +0200

    Add motor class

---
+++ bte_wind_tunnel_motor.cpp
@@ -0,0 +1,112 @@
+#include "bte_wind_tunnel_motor.h"
+
+#define POWER_FROM_PWM_A0 -1000.0
+#define POWER_FROM_PWM_A1 1.5
+#define POWER_FROM_PWM_A2 0.0
+
+#define PWM_DRIVER_MIN_POSITION 0
+#define PWM_DRIVER_MAX_POSITION 15
+
+bte_wind_tunnel_motor::bte_wind_tunnel_motor()
+{
+  	_power_flag = 0;
+  	_pwm_int = 0;
+}
+
+int8_t bte_wind_tunnel_motor::set_all_params(uint8_t motor_position, Adafruit_PWMServoDriver* pwm_driver,uint16_t min_motor,uint16_t max_motor)
+{
+	// fails if max < min
+	if( (min_motor > max_motor) || (motor_position < PWM_DRIVER_MIN_POSITION) || (motor_position > PWM_DRIVER_MAX_POSITION) ) 
+	{
+		return -1;
+	}
+	_motor_position = motor_position;
+	_pwm_driver = pwm_driver;
+	set_range(min_motor,max_motor);
+	set_pwm(min_motor);
+	
+	return 0;
+}
+
+int8_t bte_wind_tunnel_motor::set_range(uint16_t min_motor,uint16_t max_motor)
+{
+	// fails if min < max
+  	if ( min_motor > max_motor) 
+	{
+		return -1;
+	}
+	
+  	_min_motor = min_motor;
+  	_max_motor = max_motor;
+  	
+  	return 0;
+}
+
+int8_t bte_wind_tunnel_motor::set_pwm(uint16_t pwm_int)
+{
+	// fails if pwm > max or pwm > min or motor is off
+  	if( (pwm_int>_max_motor) || (pwm_int<_min_motor) || (_power_flag==0) )
+  	{
+  		return -1;
+  	}
+  	
+  	_pwm_int = pwm_int;
+  	_pwm_driver->writeMicroseconds(_motor_position,_pwm_int);
+  	
+  	return 0;
+}
+
+int8_t bte_wind_tunnel_motor::set_thrust_level(float thrust_level)
+{
+	float pwm_tmp;
+	
+	if(thrust_level>1.0 || thrust_level<0.0)
+	{
+		return -1;
+	}
+
+	pwm_tmp = (float)_min_motor + thrust_level * ((float)_max_motor - (float)_min_motor);
+	
+  	return set_pwm((uint16_t)pwm_tmp);
+}
+
+uint16_t bte_wind_tunnel_motor::get_current_pwm(void)
+{
+	return _pwm_int;
+}
+
+      
+float bte_wind_tunnel_motor::get_current_power(void)
+{
+	return get_power_from_pwm(get_current_pwm());
+}
+
+void bte_wind_tunnel_motor::power_off(void)
+{
+	//set min pwm the motor
+	_pwm_int = _min_motor;
+    _pwm_driver->writeMicroseconds(_motor_position,_pwm_int);
+}
+
+void bte_wind_tunnel_motor::enable(void)
+{
+	_power_flag = 1;
+}
+
+void bte_wind_tunnel_motor::disable(void)
+{
+	power_off();
+	_power_flag = 0;
+}
+
+
+// Function outside of the class
+
+float get_power_from_pwm(uint16_t pwm_int)
+{
+	// pwm casted in float for computation
+	float pwm_tmp = (float)pwm_int;
+  	float power = POWER_FROM_PWM_A0 + POWER_FROM_PWM_A1*pwm_tmp + POWER_FROM_PWM_A2*pwm_tmp*pwm_tmp;
+
+  	return power;
+}
---
+++ bte_wind_tunnel_motor.h
@@ -0,0 +1,110 @@
+#ifndef BTE_WIND_TUNNEL_MOTOR_H_
+  #define BTE_WIND_TUNNEL_MOTOR_H_
+
+#include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+
+
+class bte_wind_tunnel_motor{
+  /**
+   * \class bte_wind_tunnel_motor
+   * \brief class used to instantiate one motor of the wind tunnel designed by Bladetips Energy
+   */
+   
+  public:
+  
+	  bte_wind_tunnel_motor();
+	  /**
+       * \fn bte_wind_tunnel_motor()
+	   * \brief Default constructor for the bte_wind_tunnel_motor class
+       */
+       
+	  int8_t set_all_params(uint8_t motor_position, Adafruit_PWMServoDriver* pwm_driver,uint16_t min_motor,uint16_t max_motor);
+	  /**
+       * \fn set_all_params(uint8_t position_motor, Adafruit_PWMServoDriver pwm_driver,uint16_t min_motor,uint16_t max_motor)
+	   * \brief Set all the params of the wind tunnel motors
+	   * @param (uint8_t)motor_position motor plug position on the driver (0-15)
+	   * @param (Adafruit_PWMServoDriver)pwm_driver Pwm driver on which the motor is plug
+	   * @param (uint16_t)min_motor min PWM of the motor
+	   * @param (uint16_t)max_motor max PWM of the motor
+	   * @return -1 if the function fails, 0 otherwise
+       */
+       
+	  int8_t set_range(uint16_t min_motor,uint16_t max_motor);
+	  /**
+       * \fn set_range(uint16_t min_motor,uint16_t max_motor)
+	   * \brief Set all the PWM limits for the motor
+	   * @param (uint16_t)min_motor min PWM of the motor
+	   * @param (uint16_t)max_motor max PWM of the motors
+	   * @return -1 if the function fails, 0 otherwise
+       */
+       
+	  int8_t set_pwm(uint16_t pwm_int);
+	  /**
+       * \fn set_pwm(uint16_t pwm_int)
+	   * \brief Set the pwm 
+	   * @param (uint16_t)pwm_int pwm to set on the motor
+	   * @return -1 if the function fails (motor off or pwm out of bounds), 0 otherwise
+       */
+       
+	  int8_t set_thrust_level(float thrust_level);
+	  /**
+       * \fn set_thrust_level(float thrust_level)
+	   * \brief Set the wind speed for the motor
+	   * @param (float)thrust_level desired wind speed for the motor
+	   * @return -1 if the function fails (motor off or pwm corresponding to the wind speed out of bounds), 0 otherwise
+       */
+       
+       void power_off(void);
+	  /**
+       * \fn void power_off(void
+	   * \brief power off the motor without disabling it
+       */
+       
+      uint16_t get_current_pwm(void);
+      /**
+       * \fn get_current_pwm(void)
+	   * \brief Get the current PWM of the motor
+	   * @return the current PWM
+       */
+      
+      float get_current_power(void);
+      /**
+       * \fn get_current_power(void)
+	   * \brief Get the current power of the motor
+	   * @return the current power (W)
+       */
+	  
+	  void enable(void);
+	  /**
+       * \fn enable(void)
+	   * \brief enable the motor (disabled by default)
+       */
+       
+	  void disable(void);
+	  /**
+       * \fn disable(void)
+	   * \brief power off and disable the motor (disabled by default). Use of enable will be required before applying pwm to the motor
+       */
+  
+
+  private:
+	  uint8_t _motor_position;
+	  uint8_t _power_flag = 0;
+	  Adafruit_PWMServoDriver* _pwm_driver;
+	  uint16_t _min_motor;
+	  uint16_t _max_motor;
+	  uint16_t _pwm_int = 0;
+  
+  };
+  
+float get_power_from_pwm(uint16_t pwm_int);
+	/**
+	* \fn get_power_from_pwm(uint16_t pwm_int)
+	* \brief convert the PWM into power (identification done on test bench). Does not apply the PWM.
+	* @param (uint16_t)pwm_int pwm of the motor
+	* @return the power corresponding to the PWM of the motor
+	*/
+       
+
+#endif
